0da28a8c266be08cb2cb14ba9249267bb052d945
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-13 14:40:58 +0200

    added app

--- src/components/Api/fetchApi.js
+++ src/components/Api/fetchApi.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import axios from 'axios';
 
-const fetchApi = () => {
-  const API_KEY = 'af821a6be32654bd267633f7c9b9d9a0';
-  return <div></div>;
-};
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
-export default fetchApi;
+const API_KEY = 'af821a6be32654bd267633f7c9b9d9a0';
+
+export async function getMovieTrends(currentPage) {
+  try {
+    const { data } = await axios.get(
+      `trending/movie/day?api_key=${API_KEY}&page=${currentPage}`
+    );
+
+    console.log(data);
+    return data.results;
+  } catch (error) {
+    return `${error.message}, please contact the administrator`;
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <nav>
+          <NavLink className="header-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="header-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="header-link" to="/movies/:movieId">
+            MovieDetails
+          </NavLink>
+          <NavLink className="header-link" to="/movies/:movieId/cast">
+            Cast
+          </NavLink>
+          <NavLink className="header-link" to="/movies/:movieId/reviews">
+            Reviews
+          </NavLink>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
     </div>
   );
 };
