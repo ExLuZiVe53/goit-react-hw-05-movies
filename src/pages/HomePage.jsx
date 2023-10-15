a4149f271af4a314d92eab064815bb4a1fa24f2a
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-15 18:02:02 +0200

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const HomePage = () => {
+  const [films, setFilms] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAllFilms = async () => {
+      try {
+        setIsLoading(true);
+        const filmsData = await fetchFilms();
+        setFilms(filmsData);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchAllFilms();
+  }, []);
   return <div>HomePage</div>;
 };
 
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Cast from './Cast';
+import Reviews from './Reviews';
 
 const MovieDetails = () => {
-  return <div>MovieDetails</div>;
+  return (
+    <div>
+      MovieDetails
+      <Cast />
+      <Reviews />
+    </div>
+  );
 };
 
 export default MovieDetails;
